2d1e660f328ece64149c13f7411bbf98bb137d94
Dor Hayun <39770712+DorHayun@users.noreply.github.com>
2020-05-18 18:49:44 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import requests
+from unshortenit import UnshortenIt
+
+
+#get the url from user
+
+def make_rte
+while True:
+updates = requests.get("https://api.telegram.org/bot857617376:AAFX-IATq2rEKPsKcW7ylkhe_pA7jOZZeK0/getUpdates?chat_id=478322885"
+updates = updates["results"]
+for item in updates:
+  message = item["message"]["text"]
+  unshortener = UnshortenIt()
+  uri = unshortener.unshorten(message)
+  print(uri)
+  requests.get("https://api.telegram.org/bot857617376:AAFX-IATq2rEKPsKcW7ylkhe_pA7jOZZeK0/sendMessage?chat_id=478322885&text={}".format(message))
+
